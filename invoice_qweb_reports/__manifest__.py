882e621470703cc74ab728a4b979f7fd909b7fe7
Mohamed Rizwan <mohamed_rizwan@zbeanztech.com>
2023-03-02 18:26:22 +0530

    build 1.1

--- invoice_qweb_reports/__manifest__.py
+++ invoice_qweb_reports/__manifest__.py
@@ -3,17 +3,17 @@
 
 
 {
-    'name': 'Invoice Qweb Reports ',
+    'name': 'Odoo 14 Invoice PDF Reports ',
     'version': '14.0.1',
     'category': 'Invoicing Management',
+    'description': 'Create an Invoice Report by taking input of Start Date and End Date from wizard Based on Date, Multiple pdf pages will be printed in single pdf. Only invoice that are Posted and Paid be printed.',
+    'summary': 'Invoice PDF Reports for Odoo 14',
+    'sequence': '10',
     'author': 'Mohamed Rizwan, Odoo SA',
+    'license': 'LGPL-3',
+    'maintainer': 'Mohamed Rizwan',
+    'support': 'iamrizwan45@gmail.com',
     'website': 'mohamed.co.in',
-    'summary': """Create an invoice report by taking input of start date and end date from wizard""",
-    'description': """
-                Create an invoice report by taking input of start date and end date from wizard
-                Based on Date, multiple pdf pages will be printed in single pdf.
-                Only invoice that are Posted and Paid be printed.
-                """,
     'depends': ['account','base'],
     'demo': [],
     'data': [
@@ -22,9 +22,10 @@
         'report/report.xml',
         'report/report_invoice.xml',
     ],
-    'images': ['static/description/icon.png'],
-    'support': 'iamrizwan45@gmail.com',
-    'license': 'LGPL-3',
     'installable': True,
+    'application': False,
     'auto_install': False,
+    'images': ['static/description/icon.png'],
+    'qweb': [],
+
 }
